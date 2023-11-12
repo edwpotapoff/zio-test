50cc5377666d081223753f315b02b28937486cf9
Потапов Эдуард <edwpotapoff@gmail.com>
2023-11-12 19:29:12 +0300

    runtime.unsafe

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,14 @@
 import zio.actors.Actor.Stateful
 import zio.actors._
-import zio._
 import zio.Console._
+import zio.{UIO, Unsafe, ZIO}
 
 
 sealed trait Message[+_]
 case object Increase extends Message[Unit]
 case object Get      extends Message[Int]
 
-object CounterActorExample extends ZIOAppDefault {
+object CounterActorExample {
 
   // Definition of stateful actor
   val counterActor: Stateful[Any, Int, Message] =
@@ -40,10 +40,19 @@ object CounterActorExample extends ZIOAppDefault {
       _      <- actor ! Increase
       _      <- actor ! Increase
       _      <- actor ! Increase
+      _ <- ZIO.fail(new RuntimeException("first exception"))
       s      <- actor ? Get
     } yield s
 
-  override def run: ZIO[Any with ZIOAppArgs with Scope, Nothing, ExitCode] =
-    myApp.flatMap(state => printLine(s"The final state of counter: $state"))
-      .exitCode
+//  def run: ZIO[Any with ZIOAppArgs with Scope, Nothing, ExitCode] =
+//    myApp.flatMap(state => printLine(s"The final state of counter: $state"))
+//      .exitCode
+
+  def main(args: Array[String]): Unit = {
+    val runtime = zio.Runtime.default
+
+      Unsafe.unsafe { implicit unsafe =>
+        runtime.unsafe.run(myApp.tap(state => printLine(s"The final state of counter: $state"))).getOrThrowFiberFailure()
+      }
+  }
 }
